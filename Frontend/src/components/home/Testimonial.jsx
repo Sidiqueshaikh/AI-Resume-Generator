482277abe0d3ba4d4532ccd95ee9065ff8c66ccf
Sidiqueshaikh <siddiquemohammed0101@gmail.com>
2026-09-16 17:30:24 +0530

    final frontend changes

--- Frontend/src/components/home/Testimonial.jsx
+++ Frontend/src/components/home/Testimonial.jsx
@@ -63,7 +63,7 @@ const Testimonial = () => {
 
   return (
     <>
-     <div id="features" className="flex flex-col items-center my-10 scroll-mt-12">
+     <div id="testimonials" className="flex flex-col items-center my-10 scroll-mt-12">
       <div className="flex items-center gap-2 text-sm text-blue-800 bg-blue-400/10 border border-indigo-200 rounded-full px-4 py-1">
             <BookUserIcon className="size-4.5 stroke-green-600"/>
             <span>Testimonials</span>
